259625c4e333004eb313b6684ed94645429d7e1e
Oscarmrl <oscaralexandermurillo500@gmail.com>
2024-09-05 18:24:08 -0600

    Tic-Tac-Toe

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react"
+import confetti from "canvas-confetti"
+import { Square } from "./components/square.jsx"
+import { TURNS } from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
+
+
+
+
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+
+
+  const [winner, setWinner] = useState(null) // null es que no hay ganador,false es que hay un empate
+
+
+
+
+
+  const resetGame = () => {
+
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    resetGameStorage()
+  }
+
+
+
+  const updateBoard = (index) => {
+
+    // no actualizar esta posision si ya tenemos algo 
+    if (board[index] || winner) return
+
+    // actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // Cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // Guardar aqui la partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
+
+    // revisar si hay un ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // declarando empate
+    }
+  }
+
+
+
+  return (
+    <main className="board">
+      <h1>tic-tac-toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
+
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+
+                {board[index]}
+
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
+
+    </main>
+
+  )
+}
+
+export default App
---
+++ src/logic/storage/index.js
@@ -0,0 +1,11 @@
+export const saveGameToStorage = ({ board, turn }) => {
+
+    //Guardar partida
+    window.localStorage.setItem('board', JSON.stringify(board))
+    window.localStorage.setItem('turn', turn)
+}
+
+export const resetGameStorage = () => {
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+}
